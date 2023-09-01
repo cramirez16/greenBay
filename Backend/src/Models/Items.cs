78fe7a7cdb5f575ea90361d6c65a43271ace952d
cramirez16 <cramirez16a@gmail.com>
2023-09-01 18:02:40 +0200

    Added intial models.

---
+++ Backend/src/Models/Items.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Models
+{
+    public class Item
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Description { get; set; }
+
+        [Required]
+        public required string PhotoUrl { get; set; }
+
+        [Required]
+        public required decimal Price { get; set; }
+
+        public decimal Bid { get; set; } = 0m;
+        public bool IsSellable { get; set; } = true;
+    }
+}
---
+++ Backend/src/Models/User.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required decimal Money { get; set; }
+
+        [Required]
+        public required string Role { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+    }
+}
