b5b5d56b5d42745d1ae311563863a43baeb74308
cramirez16 <cramirez16a@gmail.com>
2023-09-01 23:35:26 +0200

    Updating entity to reflect the relation.

---
+++ Backend/src/Models/Bid.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Models
+{
+    public class Bid
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public decimal BidAmount { get; set; }
+        [Required]
+        public int BidderId { get; set; }
+        [ForeignKey("BidderId")]
+        public User? User { get; set; }
+        [Required]
+        public int ItemId { get; set; }
+        [ForeignKey("ItemId")]
+        public required Item Item { get; set; }
+    }
+}
