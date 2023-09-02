ac6610f4dc485bdc552253d1d860433fba779a5d
cramirez16 <cramirez16a@gmail.com>
2023-09-02 21:30:12 +0200

    Added MapperConfig.

---
+++ Backend/src/helpers/MapperConfig.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using src.Models;
+using src.Models.Dtos;
+
+namespace src.helpers
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<UserResponseDto, User>().ReverseMap();
+        }
+    }
+}
