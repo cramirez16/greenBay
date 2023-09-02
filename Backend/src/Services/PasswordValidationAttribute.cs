5b898af7c68ebacff6aeb640853e14eb190c2e34
cramirez16 <cramirez16a@gmail.com>
2023-09-02 21:31:32 +0200

    Added Password Validation Rule.

---
+++ Backend/src/Services/PasswordValidationAttribute.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Services
+{
+    public class PasswordValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            string? password = value as string;
+
+            if (string.IsNullOrEmpty(password))
+                return false;
+
+            // Check password rules
+            bool hasLetter = System.Text.RegularExpressions.Regex.IsMatch(password, "[a-zA-Z]+");
+            bool hasNumber = System.Text.RegularExpressions.Regex.IsMatch(password, "\\d+");
+            bool hasLetterCase = System.Text.RegularExpressions.Regex.IsMatch(password, "(?=.*[a-z])(?=.*[A-Z])");
+            bool hasLength = password.Length >= 6 && password.Length <= 255;
+
+            return hasLetter && hasNumber && hasLetterCase && hasLength;
+        }
+    }
+}
