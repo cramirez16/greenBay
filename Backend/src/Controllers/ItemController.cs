c003a1cd7a23ba9ac49a7dbeb1d23b6f4099e736
cramirez16 <cramirez16a@gmail.com>
2023-09-03 19:58:39 +0200

    Added Post method to add new items.

--- Backend/src/Controllers/ItemController.cs
+++ Backend/src/Controllers/ItemController.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using src.Data;
+using src.JsonConverters;
 using src.Models;
 using src.Models.Dtos;
 
@@ -36,13 +39,36 @@ namespace src.Controllers
         public async Task<IActionResult> GetItems()
         {
             List<Item>? items = await _context.TblItems
+                .Include(item => item.Seller).Include(item => item.Seller)
+                .Include(item => item.Bids)
                 .Where(item => item.IsSellable)
                 .ToListAsync();
 
 
             var itemsDto = _automapper.Map<List<ItemResponseDto>>(items);
-            _logger.LogInformation("Tickets list sent.");
-            return Ok(itemsDto);
+
+            var serializerOptions = new JsonSerializerOptions
+            {
+                Converters = { new ItemResponseDtoConverter() },
+                WriteIndented = true, // Format the JSON for readability
+                //DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull // Ignore null values
+            };
+
+            string jsonResult = JsonSerializer.Serialize(itemsDto, serializerOptions);
+
+            _logger.LogInformation("Items list sent.");
+            return Ok(jsonResult);
+        }
+
+        [HttpPost]
+        //[Authorize(Roles = "Admin,User")]
+        public async Task<IActionResult> PostItem([FromBody] ItemRequestDto ticket)
+        {
+            var ItemEntities = _automapper.Map<Item>(ticket);
+            await _context.TblItems.AddAsync(ItemEntities);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation("Created new item.");
+            return Ok();
         }
     }
 }
