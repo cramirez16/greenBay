97836c9b33df6b1778950d38e7dba6863187e3a5
cramirez16 <cramirez16a@gmail.com>
2023-09-03 20:01:40 +0200

    Add Dtos to insert a new Item and to list the actual items.

--- Backend/src/Models/Dtos/ItemRequestDto.cs
+++ Backend/src/Models/Dtos/ItemRequestDto.cs
@@ -7,19 +7,28 @@ using System.Threading.Tasks;
 
 namespace src.Models.Dtos
 {
-    public class ItemResponseDto
+    public class ItemRequestDto
     {
-        public int Id { get; set; }
+        [Required]
         public required string Name { get; set; }
-        // public required string Description { get; set; }
+
+        [Required]
+        public required string Description { get; set; }
+
+        [Required]
         public required string PhotoUrl { get; set; }
-        // public required decimal Price { get; set; }
+
+        [Required]
+        public required decimal Price { get; set; }
+
         public decimal Bid { get; set; } = 0m;
         public bool IsSellable { get; set; } = true;
+
         public DateTime CreationDate { get; set; }
+
         public DateTime UpdateDate { get; set; }
+
+        [Required]
         public int SellerId { get; set; }
-        public string? SellerName { get; set; }
-        public List<Bid>? Bids { get; set; }
     }
 }
