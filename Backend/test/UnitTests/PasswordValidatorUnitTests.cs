7bc5e56869072bdfa85a2f8088a6883e9c86bee8
cramirez16 <cramirez16a@gmail.com>
2023-09-12 23:48:31 +0200

    Added unit tests for passwordValidator.

---
+++ Backend/test/UnitTests/PasswordValidatorUnitTests.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using AutoMapper;
+using Moq;
+using src.Services;
+using src.Models;
+using src.Models.Dtos;
+using System.Security.Claims;
+
+
+namespace UnitTests
+{
+    public class PasswordValidatorUnitTests
+    {
+        [TestFixture]
+        public class PasswordValidationAttributeTests
+        {
+            private PasswordValidationAttribute _passwordValidation;
+
+            [SetUp]
+            public void Setup()
+            {
+                _passwordValidation = new PasswordValidationAttribute();
+            }
+
+            [Test]
+            public void PasswordWithoutCappitalLeterIsInvalid()
+            {
+                string password = "sñldkfjñaslkdjfñslakdf";
+                Assert.That(_passwordValidation.IsValid(password), Is.False);
+            }
+
+            [Test]
+            public void PasswordWithoutNumbersIsInvalid()
+            {
+                string password = "AAAlkdjfñslakdf";
+                Assert.That(_passwordValidation.IsValid(password), Is.False);
+            }
+
+            [Test]
+            public void PasswordWithoutLettersIsInvalid()
+            {
+                string password = "11111111111";
+                Assert.That(_passwordValidation.IsValid(password), Is.False);
+            }
+
+            [Test]
+            public void PasswordWithValidFormatIsValid()
+            {
+                string password = "Admin123";
+                Assert.That(_passwordValidation.IsValid(password), Is.True);
+            }
+
+
+        }
+    }
+}
