5c565f8874d8c0289d35682a4be93834c8d6e5ef
cramirez16 <cramirez16a@gmail.com>
2023-09-13 11:25:44 +0200

    First e2e test: snapshot.

---
+++ Frontend/src/app/greenbay/landing-page/landing-page.component.snapshot.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LandingPageComponent } from './landing-page.component';
+import { MaterialModule } from '../../material/material.module';
+
+describe('LandingPageComponent', () => {
+  let component: LandingPageComponent;
+  let fixture: ComponentFixture<LandingPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LandingPageComponent],
+      imports: [MaterialModule],
+    });
+    fixture = TestBed.createComponent(LandingPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should renders markup to snapshot', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+});
