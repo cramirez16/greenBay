f007d5a3b986512896b98eaa9f47aeb3a7cd606c
cramirez16 <cramirez16a@gmail.com>
2023-09-14 00:29:32 +0200

    Adding more unit test cases for login process.

--- Frontend/src/app/greenbay/login/login.component.spec.ts
+++ Frontend/src/app/greenbay/login/login.component.spec.ts
@@ -31,18 +31,6 @@ export class MockRouter {
   }
 }
 
-class MockAccountService {
-  private loginResponse: Observable<any>;
-
-  constructor(loginResponse: Observable<any>) {
-    this.loginResponse = loginResponse;
-  }
-
-  login(user: IUserLoginRequestDto): Observable<any> {
-    return this.loginResponse;
-  }
-}
-
 class MockLocalStorageService {
   private data: { [key: string]: any } = {};
 
@@ -74,8 +62,10 @@ type tokenContent = {
 class MockJwtDecoderService {
   public tokenContent!: tokenContent;
 
-  constructor(tokenContent: tokenContent) {
-    this.tokenContent = tokenContent;
+  constructor(tokenContent?: tokenContent) {
+    if (tokenContent) {
+      this.tokenContent = tokenContent;
+    }
   }
 
   public mockLocalStorageService = new MockLocalStorageService();
@@ -96,14 +86,7 @@ describe('LoginComponent Suscefull Login', () => {
   //const mockAccountService = new MockAccountService();
   let httpMock: HttpTestingController;
   let mockMatDialog: MatDialog;
-  const storageAfterResponse: any = {
-    userId: 1,
-    name: 'Admin',
-    isAdmin: true,
-    email: 'admin@fox.hu',
-    money: 100,
-  };
-  const mockJwtDecoderService = new MockJwtDecoderService(storageAfterResponse);
+  const mockJwtDecoderService = new MockJwtDecoderService();
 
   // Create a mock LocalStorageService instance
   //const mockLocalStorageService = new MockLocalStorageService();
@@ -161,22 +144,25 @@ describe('LoginComponent Suscefull Login', () => {
   });
 
   it('should set local storage on successful login', fakeAsync(async () => {
-    const mockResponse: IUserLoginResponseDto = {
+    const mockStorageAfterResponse: any = {
+      userId: 1,
+      name: 'Admin',
+      tokenKey: 'fakeToken',
+      isAdmin: true,
+      email: 'admin@fox.hu',
+      money: 100,
+    };
+    mockJwtDecoderService.tokenContent = mockStorageAfterResponse;
+    const mockDecodeToken: IUserLoginResponseDto = {
       tokenKey: 'fakeToken',
     };
 
     // Mock the accountService.login to return the mockResponse
-    accountService.login = jest.fn(() => of(mockResponse));
+    accountService.login = jest.fn(() => of(mockDecodeToken));
 
     // Call the component.submit method ---> will store the response data
     // in the mockedLocalStorage
-    const promise = component.submit();
-
-    // Flush the microtasks and promises (including the accountService.login call)
-    tick();
-
-    // Await the resolved promise (returned by component.submit)
-    await promise;
+    component.submit();
 
     // Reading data from LocalStorage
     const userIdInLocalStorage =
@@ -192,134 +178,158 @@ describe('LoginComponent Suscefull Login', () => {
     const moneyInLocalStorage =
       mockJwtDecoderService.mockLocalStorageService.get('money');
 
-    expect(userIdInLocalStorage).toBe(storageAfterResponse.userId);
-    expect(nameInLocalStorage).toBe(storageAfterResponse.name);
-    expect(tokenInLocalStorage).toBe(mockResponse.tokenKey);
-    expect(isAdminInLocalStorage).toBe(storageAfterResponse.isAdmin);
-    expect(emailInLocalStorage).toBe(storageAfterResponse.email);
-    expect(moneyInLocalStorage).toBe(storageAfterResponse.money);
+    expect(userIdInLocalStorage).toBe(mockStorageAfterResponse.userId);
+    expect(nameInLocalStorage).toBe(mockStorageAfterResponse.name);
+    expect(tokenInLocalStorage).toBe(mockStorageAfterResponse.tokenKey);
+    expect(isAdminInLocalStorage).toBe(mockStorageAfterResponse.isAdmin);
+    expect(emailInLocalStorage).toBe(mockStorageAfterResponse.email);
+    expect(moneyInLocalStorage).toBe(mockStorageAfterResponse.money);
   }));
 });
 
-// describe('LoginComponent with Network Error', () => {
-//   let component: LoginComponent;
-//   let fixture: ComponentFixture<LoginComponent>;
-//   let mockAccountService: MockAccountService;
-//   let mockMatDialog: MatDialog;
+class MockAccountService {
+  private loginResponse: Observable<any>;
 
-//   beforeEach(async () => {
-//     // intializing mockAccountService for the specific case
-//     const errorResponse = new HttpErrorResponse({
-//       error: 'Unauthorized',
-//       status: 0,
-//       statusText: 'Unauthorized',
-//     });
-//     mockAccountService = new MockAccountService(throwError(errorResponse));
+  constructor(loginResponse: Observable<any>) {
+    this.loginResponse = loginResponse;
+  }
 
-//     // Moking MatDialog
-//     mockMatDialog = {
-//       open: jest.fn(),
-//     } as unknown as MatDialog;
+  login(user: IUserLoginRequestDto): Observable<any> {
+    return this.loginResponse;
+  }
+}
 
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         ReactiveFormsModule,
-//         MaterialModule,
-//         RouterTestingModule,
-//       ],
-//       declarations: [LoginComponent],
-//       providers: [
-//         LocalStorageService,
-//         UserValidationService,
-//         { provide: AccountService, useValue: mockAccountService },
-//         { provide: MatDialog, useValue: mockMatDialog },
-//       ],
-//     }).compileComponents();
-//   });
+//------------------------------------------------------
+//                       Test 2
+//------------------------------------------------------
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LoginComponent);
-//     component = fixture.componentInstance;
-//   });
+describe('LoginComponent with Network Error', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let mockAccountService: MockAccountService;
+  let mockMatDialog: MatDialog;
 
-//   it('should handle Network Error', () => {
-//     const userLogin: IUserLoginRequestDto = {
-//       email: 'cramirez@gmail.com',
-//       password: '123456Aa',
-//     };
-//     // calling submit login method in login component.
-//     component.submit(userLogin);
+  beforeEach(async () => {
+    // intializing mockAccountService for the specific case
+    const errorResponse = new HttpErrorResponse({
+      error: 'Unauthorized',
+      status: 0,
+      statusText: 'Unauthorized',
+    });
+    mockAccountService = new MockAccountService(throwError(errorResponse));
 
-//     // expected display dialog content on error handling.
-//     const dialogData = {
-//       title: '',
-//       description: 'Network Error',
-//     };
+    // Moking MatDialog
+    mockMatDialog = {
+      open: jest.fn(),
+    } as unknown as MatDialog;
 
-//     // Assertion
-//     expect(mockMatDialog.open).toHaveBeenCalledWith(MessagePopupComponent, {
-//       data: dialogData,
-//     });
-//   });
-// });
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, MaterialModule, RouterTestingModule],
+      declarations: [LoginComponent],
+      providers: [
+        LocalStorageService,
+        UserValidationService,
+        { provide: AccountService, useValue: mockAccountService },
+        { provide: MatDialog, useValue: mockMatDialog },
+      ],
+    }).compileComponents();
+  });
 
-// describe('LoginComponent with Unauthorized', () => {
-//   let component: LoginComponent;
-//   let fixture: ComponentFixture<LoginComponent>;
-//   let mockAccountService: MockAccountService;
-//   let mockMatDialog: MatDialog;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+  });
 
-//   beforeEach(async () => {
-//     // intializing mockAccountService for the specific case
-//     const errorResponse = new HttpErrorResponse({
-//       error: 'Unauthorized',
-//       status: 401,
-//       statusText: 'Unauthorized',
-//     });
-//     mockAccountService = new MockAccountService(throwError(errorResponse));
+  it('should handle Network Error', () => {
+    // Set initial model values
+    component.email.setValue('admin@fox.hu');
+    component.password.setValue('Admin123');
 
-//     mockMatDialog = {
-//       open: jest.fn(),
-//     } as unknown as MatDialog;
+    // Mark the form controls as pristine and untouched
+    component.email.markAsTouched();
+    component.email.markAsPristine();
+    component.password.markAsTouched();
+    component.password.markAsPristine();
 
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         ReactiveFormsModule,
-//         MaterialModule,
-//         RouterTestingModule,
-//       ],
-//       declarations: [LoginComponent],
-//       providers: [
-//         LocalStorageService,
-//         UserValidationService,
-//         { provide: AccountService, useValue: mockAccountService },
-//         { provide: MatDialog, useValue: mockMatDialog },
-//       ],
-//     }).compileComponents();
-//   });
+    // calling submit login method in login component.
+    component.submit();
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LoginComponent);
-//     component = fixture.componentInstance;
-//   });
+    // expected display dialog content on error handling.
+    const dialogData = {
+      title: '',
+      description: 'Network Error',
+    };
 
-//   it('should handle Unauthorized', () => {
-//     const userLogin: IUserLoginRequestDto = {
-//       email: 'cramirez@gmail.com',
-//       password: '123456Aa',
-//     };
-//     // calling submit login method in login component.
-//     component.submit(userLogin);
+    // Assertion
+    expect(mockMatDialog.open).toHaveBeenCalledWith(MessagePopupComponent, {
+      data: dialogData,
+    });
+  });
+});
 
-//     // expected display dialog content on error handling.
-//     const dialogData = {
-//       title: 'Login failed',
-//       description: 'Wrong credentials',
-//     };
+//------------------------------------------------------
+//                       Test 3
+//------------------------------------------------------
 
-//     // Assertion
-//     expect(mockMatDialog.open).toHaveBeenCalledWith(MessagePopupComponent, {
-//       data: dialogData,
-//     });
-//   });
-// });
+describe('LoginComponent with Unauthorized', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let mockAccountService: MockAccountService;
+  let mockMatDialog: MatDialog;
+
+  beforeEach(async () => {
+    // intializing mockAccountService for the specific case
+    const errorResponse = new HttpErrorResponse({
+      error: 'Unauthorized',
+      status: 401,
+      statusText: 'Unauthorized',
+    });
+    mockAccountService = new MockAccountService(throwError(errorResponse));
+
+    mockMatDialog = {
+      open: jest.fn(),
+    } as unknown as MatDialog;
+
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, MaterialModule, RouterTestingModule],
+      declarations: [LoginComponent],
+      providers: [
+        LocalStorageService,
+        UserValidationService,
+        { provide: AccountService, useValue: mockAccountService },
+        { provide: MatDialog, useValue: mockMatDialog },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should handle Unauthorized', () => {
+    // Set initial model values
+    component.email.setValue('admin@fox.hu');
+    component.password.setValue('Admin123');
+
+    // Mark the form controls as pristine and untouched
+    component.email.markAsTouched();
+    component.email.markAsPristine();
+    component.password.markAsTouched();
+    component.password.markAsPristine();
+
+    // calling submit login method in login component.
+    component.submit();
+
+    // expected display dialog content on error handling.
+    const dialogData = {
+      title: 'Login failed',
+      description: 'Wrong credentials',
+    };
+
+    // Assertion
+    expect(mockMatDialog.open).toHaveBeenCalledWith(MessagePopupComponent, {
+      data: dialogData,
+    });
+  });
+});
