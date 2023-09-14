64f02234ceeba6df38aca6a555a4eb88d7166c29
cramirez16 <cramirez16a@gmail.com>
2023-09-14 23:44:29 +0200

    Fixed unit test.

--- Frontend/src/app/greenbay/generic-banner/generic-banner.component.spec.ts
+++ Frontend/src/app/greenbay/generic-banner/generic-banner.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GenericBannerComponent } from './generic-banner.component';
+import { MaterialModule } from '../../material/material.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('GenericBannerComponent', () => {
   let component: GenericBannerComponent;
@@ -8,7 +10,12 @@ describe('GenericBannerComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GenericBannerComponent]
+      declarations: [GenericBannerComponent],
+      imports: [MaterialModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+      ],
     });
     fixture = TestBed.createComponent(GenericBannerComponent);
     component = fixture.componentInstance;
