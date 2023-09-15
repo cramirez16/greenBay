129ce2a1083a71905ded2fef0e1f738857714a93
cramirez16 <cramirez16a@gmail.com>
2023-09-15 11:35:55 +0200

    Renaming folders.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './greenbay/landing-page/landing-page.component';
-import { LoginComponent } from './greenbay/login/login.component';
-import { RegisterComponent } from './greenbay/register/register.component';
-import { ItemsComponent } from './greenbay/items/items.component';
-import { MyProfileComponent } from './greenbay/my-profile/my-profile.component';
-import { CreateItemComponent } from './greenbay/create-item/create-item.component';
-import { DetailedViewComponent } from './greenbay/detailed-view/detailed-view.component';
+import { LoginComponent } from './greenbay/user-login/login.component';
+import { RegisterComponent } from './greenbay/user-register/register.component';
+import { ItemsComponent } from './greenbay/item-list/items.component';
+import { CreateItemComponent } from './greenbay/item-create/create-item.component';
+import { DetailedViewComponent } from './greenbay/item-detailed-view/detailed-view.component';
 import { SearchComponent } from './greenbay/search/search.component';
+import { UserDeleteComponent } from './greenbay/user-delete/user-delete.component';
+import { UserUpdateComponent } from './greenbay/user-update/user-update.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'items', component: ItemsComponent },
-  { path: 'my_profile', component: MyProfileComponent },
+  { path: 'user-delete', component: UserDeleteComponent },
+  { path: 'user-update', component: UserUpdateComponent },
   { path: 'search', component: SearchComponent },
   {
     path: 'create-item',
--- Frontend/src/app/greenbay/greenbay.module.ts
+++ Frontend/src/app/greenbay/greenbay.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './user-login/login.component';
 import { MaterialModule } from '../material/material.module';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 import { DialogPopupComponent } from './dialog-popup/dialog-popup.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './user-register/register.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -14,12 +14,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AccountService } from '../services/account.service';
 import { MessagePopupComponent } from './message-popup/message-popup.component';
-import { ItemsComponent } from './items/items.component';
-import { MyProfileComponent } from './my-profile/my-profile.component';
-import { CreateItemComponent } from './create-item/create-item.component';
-import { DetailedViewComponent } from './detailed-view/detailed-view.component';
+import { ItemsComponent } from './item-list/items.component';
+import { CreateItemComponent } from './item-create/create-item.component';
+import { DetailedViewComponent } from './item-detailed-view/detailed-view.component';
 import { SearchComponent } from './search/search.component';
 import { GenericBannerComponent } from './generic-banner/generic-banner.component';
+import { UserDeleteComponent } from './user-delete/user-delete.component';
+import { UserUpdateComponent } from './user-update/user-update.component';
 
 @NgModule({
   declarations: [
@@ -28,9 +29,10 @@ import { GenericBannerComponent } from './generic-banner/generic-banner.componen
     LandingPageComponent,
     DialogPopupComponent,
     MessagePopupComponent,
-    MyProfileComponent,
     CreateItemComponent,
     GenericBannerComponent,
+    UserDeleteComponent,
+    UserUpdateComponent,
   ],
   imports: [
     CommonModule,
